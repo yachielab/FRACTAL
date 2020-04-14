8d76ebb503c4d193c57f1dc3fa50d086ea068b8c
UTNK <0ratnaramatn1n@gmail.com>
2020-04-14 13:12:41 +0900

    debug

--- example/script/ml_raxml.sh
+++ example/script/ml_raxml.sh
@@ -1,3 +1,3 @@
 FASTA_file_path=$1
 raxmlHPC-SSE3 -s ${FASTA_file_path} -n test -m GTRGAMMA -p 1 > /dev/null    # change this line
-mv test_bestTree.raxml ${FASTA_file_path}.tree                              # change this line
+mv RAxML_bestTree.test ${FASTA_file_path}.tree                              # change this line
