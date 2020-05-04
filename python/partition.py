e4c4e5a751ab9700e6eb5d65e65dd26d1255d4f7
UTNK <0ratnaramatn1n@gmail.com>
2020-05-04 14:28:08 +0900

    git push

--- python/partition.py
+++ python/partition.py
@@ -200,7 +200,7 @@ def qsub_prep(COMMAND, QSUBDIR, DIRdict):
             qf.write("LD_LIBRARY_PATH={}\n".format(LD_LIBRARY_PATH))
             command_list=COMMAND.split()[0:1]+[DIRdict[key][0]]+COMMAND.split()[3:]
             command=""
-            for arg in command: command += arg + " "
+            for arg in command_list: command += arg + " "
             qf.write(command) 
 
 def tiny_tree(INPUTfa,OUTPUTnwk):
