ecf22ca2aca9b892fec9e06a9cc9dd334afce1e2
UTNK <0ratnaramatn1n@gmail.com>
2020-05-05 08:42:44 +0900

    git push

--- python/FRACluster.py
+++ python/FRACluster.py
@@ -129,6 +129,7 @@ def FRACluster(COMMAND, WD, MAX_ITERATION, SUBSAMPLE_SIZE, NODESDIR, THRESHOLD, 
         #partition .fasta#
         ##################
         os.chdir(WD)
+        print("sequence count",seq_count,Nseq_in_largest_subclade)
         DIRdict=partition.partition_fasta(WD+"/INPUT.fa",NUMFILE,NODESDIR,WD,WD+"/PARTITION/partition"+str(min(i,MAX_ITERATION-1))+".out","PARTITION.info","UPSTREAM.nwk",WD+"/ANCSEQ/RAxML_marginalAncestralStates.ANCSEQ", WD+"/ANCSEQ/RAxML_nodeLabelledRootedTree.ANCSEQ", WD+"/SUBSAMPLE/RENAMED_"+str(i)+".fa",ROOTING)
         partition.qsub_prep(COMMAND, QSUBDIR, DIRdict)
         ##################
