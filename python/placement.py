71ac4baa6285ba102c3f72e6b1458ad4d591e796
UTNK <0ratnaramatn1n@gmail.com>
2020-05-14 21:34:14 +0900

    git push

--- python/placement.py
+++ python/placement.py
@@ -183,6 +183,7 @@ def distributed_placement(  WD, EPANG, refseq, reftree, model,
                         # Conduct HMM alignment
                         handle.write(hmm_aligner+
                             " --outformat afa"                         +
+                            " --allcol "                               +
                             " --trim "                                 +
                             " --mapali " + refseq + " "                +
                             refseq+".hmm "                             +
