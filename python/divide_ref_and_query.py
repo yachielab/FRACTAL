e7917135e9cc90c0b26f450b80d4caa94ba4668a
UTNK <0ratnaramatn1n@gmail.com>
2020-05-15 11:22:59 +0900

    debug

--- python/divide_ref_and_query.py
+++ python/divide_ref_and_query.py
@@ -4,7 +4,7 @@ import sys
 def divide_fasta_into_ref_and_query(ref_query, ref):
     
     with    open(ref,'r')                as ihandle       :
-        refseq_itr         = SeqIO.parse(ihandle, "phylip")
+        refseq_itr         = SeqIO.parse(ihandle, "fasta")
         refseq_name_set    = set()
         for record in refseq_itr:
             refseq_name_set.add(record.name)
@@ -12,7 +12,7 @@ def divide_fasta_into_ref_and_query(ref_query, ref):
     with    open(ref_query,'r')          as ihandle       ,\
             open(ref_query+".ref",'w')   as ohandle_ref   ,\
             open(ref_query+".query",'w') as ohandle_query :
-        ref_query_itr      = SeqIO.parse(ihandle,"phylip")
+        ref_query_itr      = SeqIO.parse(ihandle,"fasta")
         ref_query_name_set = {}
         for record in ref_query_itr:
             if(record.name in refseq_name_set):
