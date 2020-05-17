734126432a1498fc4baf886a284edf7afc2839c5
UTNK <0ratnaramatn1n@gmail.com>
2020-05-17 09:58:30 +0900

    debug

--- python/FRACluster.py
+++ python/FRACluster.py
@@ -97,7 +97,11 @@ def FRACluster(ARGVS, WD, MAX_ITERATION, SUBSAMPLE_SIZE, NODESDIR, THRESHOLD, TH
                             " -q "  + SOFTWARE                   +
                             " -s "  + ALIGNER                    ,
                             shell   = True                       )
-            partition.rooting_and_remove(WD+"/INPUT.fa.aligned.tree",WD+"/TERMINAL.nwk","root")
+            partition.rooting_and_remove(
+                WD+"/INPUT.fa.aligned.tree",
+                WD+"/TERMINAL.nwk",
+                "root"
+            )
 
     # call fractal FRACTAL # don't forget to change!!!!!
     elif(NODE_COUNT>1 and seq_count<=INIT_SEQ_COUNT//NODE_COUNT): 
@@ -352,20 +356,28 @@ def FRACluster(ARGVS, WD, MAX_ITERATION, SUBSAMPLE_SIZE, NODESDIR, THRESHOLD, TH
         #delete files    #
         ##################
 
-
-        os.remove("INPUT.fa")
-        os.remove("ITERATION.fa")
-        os.remove("tmp.nwk")
-        try: 
-            os.remove("INPUT.fa.aligned")
-            os.remove("INPUT.fa.aligned.problematic")
-            os.remove("SUBSAMPLE.fa.aligned")
-        shutil.rmtree("ANCSEQ")
-        shutil.rmtree("EPANG")
-        shutil.rmtree("PARAM")
-        shutil.rmtree("PARTITION")
-        shutil.rmtree("SUBSAMPLE")
-        shutil.rmtree("TREE")
+        filenames = [
+            "INPUT.fa",
+            "ITERATION.fa",
+            "tmp.nwk",
+            "INPUT.fa.aligned",
+            "INPUT.fa.aligned.problematic",
+            "SUBSAMPLE.fa.aligned",
+        ]
+        dirnames = [
+            "ANCSEQ",
+            "EPANG",
+            "PARAM",
+            "PARTITION",
+            "SUBSAMPLE",
+            "TREE",
+        ]
+        for filename in filenames:
+            try: 
+                os.remove(filename)
+        for dirname in dirnames:
+            try: 
+                shutil.rmtree(dirname)
         
     
     elapsed_time=time.time()-start
