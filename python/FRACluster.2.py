43288b1869d6958068ba93d2743c6c3a58d61509
UTNK <0ratnaramatn1n@gmail.com>
2020-07-25 14:30:03 +0900

    debug

--- python/FRACluster.2.py
+++ python/FRACluster.2.py
@@ -253,7 +253,7 @@ def FRACluster(ARGVS, WD, MAX_ITERATION, SUBSAMPLE_SIZE, NODESDIR, THRESHOLD, TH
                 placement.distributed_placement(
                     WD                                                  , 
                     EPANG                                               ,  
-                    WD+"/SUBSAMPLE/RENAMED_"+str(i)+".fa.gz.aligned.gz" , 
+                    WD+"/SUBSAMPLE/RENAMED_"+str(i)+".fa.gz.aligned"    , 
                     TREE_FILE                                           , 
                     WD+"/PARAM/RAxML_info.PARAM_"+str(i)                , 
                     QUERY_EDITS                                         , 
