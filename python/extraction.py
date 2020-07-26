03d00a62476730de86f10668befcac147436b66b
UTNK <0ratnaramatn1n@gmail.com>
2020-07-26 13:38:40 +0900

    debug

--- python/extraction.py
+++ python/extraction.py
@@ -44,11 +44,10 @@ def all_child_true(clade, name_to_numtips):
     return True
 
 def create_tree(tree, name_to_numtips):
-    #init_clade = Phylo.BaseTree.Clade(name="origin")
-    newtree = Phylo.BaseTree.Tree(tree.clade)
+    init_clade = Phylo.BaseTree.Clade(name="origin")
+    newtree = Phylo.BaseTree.Tree(init_clade)
     stack=[]
-    for clade in tree.clade.clades:
-        stack.append([clade, tree.clade])
+    stack.append([copy.deepcopy(tree.clade),init_clade])
     while(stack!=[]):
         statepair=stack.pop()
         cstate=statepair[0]
@@ -63,7 +62,7 @@ def create_tree(tree, name_to_numtips):
             for child in cstate.clades:
                 if(name_to_numtips[child.name]):
                     stack.append([child,parent])
-    return newtree
+    return Phylo.BaseTree.Tree(newtree.clade.clades[0])
 
 def fasta_extraction(fastafile,nameset,ext_fastafile):
     with gzip.open(fastafile,'rt') as ihandle, gzip.open(ext_fastafile,'wt') as ohandle:
