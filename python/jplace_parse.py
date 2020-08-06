3d8caa9b5ab385866bd3f34e3a841eeae4fc41b8
UTNK <0ratnaramatn1n@gmail.com>
2020-08-06 16:09:35 +0900

    debug

--- python/jplace_parse.py
+++ python/jplace_parse.py
@@ -57,7 +57,8 @@ def parse_jplace(fname, placement_method,seed):
         elif(placement_method=="epa_MP"):
             equally_parsimonious_edge_list = list('{'+str(pl[0])+'}' for pl in placement['p'])
             #edge = random.choice(equally_parsimonious_edge_list)
-            edge = tree.common_ancestor(equally_parsimonious_edge_list).name
+            edge_str = tree.common_ancestor(equally_parsimonious_edge_list).name
+            edge = int(edge_str.split('{')[1].split('}')[0])
             name = placement['n'][0]
         if(name!='root'):
             placement_list[edge].append(name)
