72919b32b16b7892de5e6f1c7206721d265eb285
UTNK <0ratnaramatn1n@gmail.com>
2020-08-14 10:24:37 +0900

    debug

--- shell/SUPERVISE.sh
+++ shell/SUPERVISE.sh
@@ -1,7 +1,7 @@
 echo -n "Lineage reconstruction started:  "
 date
 
-if [ $# -ne 23 ]; then
+if [ $# -ne 24 ]; then
   echo "args:$#" 1>&2
   echo "SUPERVISE.sh: wrong number of arguments!" 1>&2
   exit 1
