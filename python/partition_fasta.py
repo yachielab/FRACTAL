dd4a08d0d7925f82a2df5a505be03f48072ab43b
UTNK <0ratnaramatn1n@gmail.com>
2020-09-18 09:57:31 +0900

    debug

--- python/partition_fasta.py
+++ python/partition_fasta.py
@@ -23,10 +23,10 @@ def classify_sequences(inputFASTA_filehandle, outputFASTA_filehandlelist, seqnam
             outhandle = dirpath2handle[seqname2dirpath[record.id]]
             SeqIO.write(record, outhandle, "fasta")
 
-def partition_fasta(inputFASTA_filepathlist, outputFASTA_dirpathlist, seqname2dirpath):
+def partition_fasta(inputFASTA_filepathlist, outputFASTA_dirpathlist, seqname2dir_filepath):
 
     seqname2dirpath = {}
-    with open(seqname2dirpath, 'r') as dicst:
+    with open(seqname2dir_filepath, 'r') as dicst:
         for line in dicst:
             line     = line.split("\n")[0]
             seqname  = line.split("\t")[0]
@@ -64,5 +64,5 @@ if __name__ == "__main__":
     partition_fasta(
         inputFASTA_filepathlist = sys.argv[1].split(":"), 
         outputFASTA_dirpathlist = sys.argv[2].split(":"), 
-        seqname2dirpath         = sys.argv[3]
+        seqname2dir_filepath    = sys.argv[3]
         )
