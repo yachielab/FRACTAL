ebc8107cf73bf9a60e8494e8bfea5876ccc19a06
UTNK <0ratnaramatn1n@gmail.com>
2020-09-19 21:35:50 +0900

    debug

--- python/FRACluster.py
+++ python/FRACluster.py
@@ -248,6 +248,7 @@ def FRACluster(ARGVS, WD, MAX_ITERATION, SUBSAMPLE_SIZE, NODESDIR, THRESHOLD, TH
             else:
                 os.mkdir(example_infile_fpath+".split")
                 for infile_path in infile_pathlist:
+                    fpath2seqcount[example_infile_fpath+".split"+infile_path.split("/")[-1]] = fpath2seqcount[infile_path]
                     shutil.move(infile_path, example_infile_fpath+".split")
                 splitted_dirpath = example_infile_fpath+".split"
 
