0524cfb9a6c4c7dc1dacf94f9bbbbc64e90c6d64
UTNK <0ratnaramatn1n@gmail.com>
2020-11-12 14:55:23 +0900

    debug

--- python/placement.py
+++ python/placement.py
@@ -421,10 +421,10 @@ def distributed_placement(  WD, EPANG, refseq, reftree, model,
                                 )
                         elif(ALIGNED=="aligned"): # for aligned sequences
                             handle.write(
-                                "cat "+refseq+" "+
+                                "(cat "+refseq+"; cat"+
                                 queryfile        +
                                 gunzipcommand    +
-                                "| sed 's/\./N/g'> "+outdir+"/EPANG"+str(i)+"/"+filename+"/ref_query.gap2N.fa\n"
+                                ")| sed 's/\./N/g'> "+outdir+"/EPANG"+str(i)+"/"+filename+"/ref_query.gap2N.fa\n"
                             )
                         handle.write(
                             RAXMLSEQ                                      +
